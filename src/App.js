cc75c8f56cec55eb59c409b706b6df54ba6a64f4
BuildTools <unconfigured@null.spigotmc.org>
2023-10-25 16:01:56 +0900

    change URL

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function Page({title}) {
         setCookie('submitted',b,{maxAge:'2592000'})
         console.log('submit')
     }
-    if(cookies.submitted) {
+    if(false) {
         return <Thanks title={title}/>
     } else {
         return <MainForm title = {title} setSubmitted={setter}/>
